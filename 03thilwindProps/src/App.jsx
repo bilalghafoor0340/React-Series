c28bdd9e756b7ec2b72669f1813049a08c83aa07
Bilal Ghafoor <bilalghafoor1020@gmail.com>
2026-06-19 11:22:54 +0500

    Describe your changes

--- 03thilwindProps/src/App.jsx
+++ 03thilwindProps/src/App.jsx
@@ -4,9 +4,7 @@ import Card from "./Components/Card";
 function App() {
   return (
     <>
-      <h1 className="bg-green-400 p-4">Hello world</h1>
-      <Card username="bilal" />
-      <Card />
+      
     </>
   );
 }
--- counter02/src/App.jsx
+++ counter02/src/App.jsx
@@ -1,14 +1,38 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
+import { useState } from "react";
 import "./App.css";
 
+
+
 function App() {
+
+ let [counter, setCounter] = useState(10)
+
+  const addValue = () => {
+   if(counter < 25){
+    counter = counter + 1
+    setCounter(counter)
+   }
+    
+  }
+
+  let removeValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1)
+    }
+    // setCounter(counter - 1)
+  }
   
 
   return (
     <>
-    
+    <h1>Lets build a  Counter APP</h1>
+    <h2>Count Here : {counter}</h2>
+
+
+    <button onClick={addValue}>Add Value {counter}</button>
+    <br />
+    <button onClick={removeValue}>Subtract Value {counter}</button>
+    <p>footer {counter}</p>
     </>
   );
 }
